4d8acefa548156eed38ce87b780f70271fa6b21f
Jan-24 <1900125416@qq.com>
2024-09-22 15:42:08 +0800

    修改share.js

--- src/utils/set-all-share.js
+++ src/utils/set-all-share.js
@@ -4,7 +4,8 @@ import {
 export default () => {
 	wx.onAppRoute(e => {
 		let page = getCurrentPages()[getCurrentPages().length - 1]
-		if (page && !page.data.isUseShare) {
+		console.log("page", page, page.route, page.route.includes('xunbao/'))
+		if (page.route.includes('xunbao/') && page && !page.data.isUseShare) {
 			page.onShareAppMessage = e => {
 				const config = getConfig();
 				console.log(config, '---configconfigconfig----');
