8577ed1faf28adeba3c7617b08f333a07c2a77e0
17it <790356596@qq.com>
2024-09-23 18:03:59 +0800

    分享带邀请码

--- src/utils/set-all-share.js
+++ src/utils/set-all-share.js
@@ -1,19 +1,25 @@
 import {
-	getConfig
+  getConfig
 } from '@/pages-game/xunbao/api/project.config.js'
+import { addUserIdWhenShare } from '@/utils/index'
+
 export default () => {
-	wx.onAppRoute(e => {
-		let page = getCurrentPages()[getCurrentPages().length - 1]
-		console.log("page", page, page.route, page.route.includes('xunbao/'))
-		if (page.route.includes('xunbao/') && page && !page.data.isUseShare) {
-			page.onShareAppMessage = e => {
-				const config = getConfig();
-				console.log(config, '---configconfigconfig----');
-				return config.SHAREINFO
-			}
-		}
-	})
+  wx.onAppRoute(e => {
+    const page = getCurrentPages()[getCurrentPages().length - 1]
+    if (page.route.includes('xunbao/') && page && !page.data.isUseShare) {
+      page.onShareAppMessage = e => {
+        const config = getConfig()
+        console.log('configconfigconfig', config)
+
+        const shareInfo = config.SHAREINFO
+        shareInfo.path = addUserIdWhenShare(shareInfo.path)
+
+        return shareInfo
+      }
+    }
+  })
 }
+
 // import config from '../../api/project.config.js'
 // export default () => {
 //   wx.onAppRoute(e => {
